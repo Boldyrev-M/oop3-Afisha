0b82c50a25a1665095ce1ca4ffb351afebb64b28
Maxim Boldyrev <maxikb@mail.ru>
2020-11-12 23:24:45 +0300

    manager constructor changed, tests added

--- src/main/java/ru/netology/domain/MovieManager.java
+++ src/main/java/ru/netology/domain/MovieManager.java
@@ -2,30 +2,29 @@ package ru.netology.domain;
 
 public class MovieManager {
     private Movie[] movies = new Movie[0];
+    private int lastMovies;
+
+    public MovieManager() {
+        lastMovies = 10;
+    }
+
+    public MovieManager(int lastMovies) {
+        this.lastMovies = lastMovies;
+    }
 
     public void add(Movie newMovie) {
         int length = movies.length + 1;
         Movie[] tmp = new Movie[length];
 
-        //        for (int i = 0; i < movies.length; i++) {
-        //            tmp[i] = movies[i];
-        //        }
         System.arraycopy(movies, 0, tmp, 0, movies.length);
-        //               ^source           ^destination   ^how many
 
         int lastIndex = tmp.length - 1;
         tmp[lastIndex] = newMovie;
         movies = tmp;
     }
 
-    public Movie[]  showLastMovies() {
-        return showLastMovies(10);
-    }
-
-    public Movie[] showLastMovies(int number) {
-
-//        if (number < movies.length)
-        number = Math.min(number, movies.length);
+    public Movie[] showLastMovies() {
+        int number = Math.min(lastMovies, movies.length);
         Movie[] resultMovies = new Movie[number];
 
         for (int i = 0; i < number; i++) {
